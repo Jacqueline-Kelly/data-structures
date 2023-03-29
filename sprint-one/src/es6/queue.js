3975323fbce54dac24744b55bcee2784e33711dc
Jacqueline-Kelly <jacqueline.kelly.2016@gmail.com>
2023-03-29 15:25:28 -0700

    es6 queue functional

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
+  enqueue(value) {
+    var keyArr = Object.keys(this.storage);
+    if (keyArr.length === 0) {
+      this.storage[0] = value;
+      return;
+    }
+    this.storage[keyArr.slice(-1)[0] + 1] = value;
+    return;
+  }
+
+  dequeue() {
+    var keyArr = Object.keys(this.storage);
+    if (keyArr.length === 0) {
+      return;
+    }
+    var firstInd = keyArr.slice(0)[0];
+    var firstEl = this.storage[firstInd];
+    delete this.storage[firstInd];
+    return firstEl;
+  }
 }
