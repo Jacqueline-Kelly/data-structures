45bd2d07ce9b72922737377488f342019b755b76
Jacqueline-Kelly <jacqueline.kelly.2016@gmail.com>
2023-03-29 12:28:37 -0700

    functional stack is functional

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,28 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var keysArr = Object.keys(storage);
+    if (keysArr.length === 0) {
+      storage[0] = value;
+      return;
+    }
+    storage[Number(keysArr.slice(-1)[0]) + 1] = value;
+    return;
   };
 
   someInstance.pop = function() {
+    var keysArr = Object.keys(storage);
+    if (keysArr.length === 0) {
+      return;
+    }
+    var poppedEl = storage[keysArr.slice(-1)[0]];
+    var poppedInd = String(keysArr.slice(-1)[0]);
+    delete storage[poppedInd];
+    return poppedEl;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
